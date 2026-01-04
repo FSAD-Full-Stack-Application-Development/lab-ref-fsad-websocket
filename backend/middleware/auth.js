bdadb8fbc4cb66a8abf959a3a885d18e4fd2cbe1
limhpone <158800872+limhpone@users.noreply.github.com>
2026-01-04 18:02:44 +0700

    lab-ref-fsad-websocket

---
+++ backend/middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (headerName) {
+  return function (req, res, next) {
+    const authHeader = req.headers['authorization']
+
+    if (!authHeader) {
+      return res.status(400).json({
+        error: `Missing required header: ${headerName}`
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+    console.log("test", token)
+
+    jwt.verify(token, "jwtsecretkey", (err, dec) => {
+        if (err) {
+            return res.status(401).json({error: 'Invalid token'})
+        }
+        next();
+    })
+  };
+};
+
+
+
+"hello world"
+
+result = ["hello", "world"]
+result[0]
+result[1]
+
+
+
+
